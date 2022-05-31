b187f9019bc16923b722246a59547006f5021a11
Djengiz Ibrqmov <djengizibrqmov@gmail.com>
2022-05-31 21:42:54 +0300

    Intruduced Register and Listing users

---
+++ FitnessApp/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿namespace FitnessApp.Controllers
+{
+    using FitnessApp.Dto;
+    using FitnessApp.Models;
+    using FitnessApp.Services;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly IUsersService usersService;
+        public UsersController(UserManager<ApplicationUser> userManager, IUsersService usersService)
+        {
+            this.userManager = userManager;
+            this.usersService = usersService;
+        }
+
+        [HttpGet]
+        public IEnumerable<UserDTO> Users()
+        {
+            var users = usersService.GetUsers();
+
+            return users;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegisterInputModel userInput)
+        {
+            var user = new ApplicationUser()
+            {
+                Email = userInput.Email,
+                UserName = userInput.Username
+            };
+
+            var result = await this.userManager.CreateAsync(user, userInput.Password);
+            return this.Ok(result);
+        }
+    }
+}
