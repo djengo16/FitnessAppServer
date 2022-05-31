e1b8049a194b92ec3c48476ced07942fa11d0a38
Djengiz Ibrqmov <djengizibrqmov@gmail.com>
2022-05-31 13:16:48 +0300

    Removed from summaries

--- FitnessApp/Controllers/WeatherForecastController.cs
+++ FitnessApp/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace FitnessApp.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
