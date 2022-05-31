be3aabd455e73d5166120aa0720fc4b13ef3deda
Djengiz Ibrqmov <djengizibrqmov@gmail.com>
2022-05-31 13:29:55 +0300

    Removed Freezing

--- FitnessApp/Controllers/WeatherForecastController.cs
+++ FitnessApp/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace FitnessApp.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
