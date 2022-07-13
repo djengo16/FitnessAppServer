fc002bb270af38ee1c353e190d1b9cfdc7917159
Djengiz Ibrqmov <djengizibrqmov@gmail.com>
2022-07-13 17:43:20 +0300

    Remove unneccesery properties from user dto

--- FitnessApp/Dto/Users/UserRegisterInputModel.cs
+++ FitnessApp/Dto/Users/UserRegisterInputModel.cs
@@ -8,17 +8,8 @@
         public string Email { get; set; } = "";
 
         [Required]
-        [StringLength(100, ErrorMessage = "Username should be at least 6 characters long!", MinimumLength = 6)]
-        public string Username { get; set; }
-
-        [Required]
         [DataType(DataType.Password)]
         [StringLength(200, ErrorMessage = "Password should be at least 6 characters long!", MinimumLength = 6)]
         public string Password { get; set; }
-
-        [Required]
-        [DataType(DataType.Password)]
-        [Compare("Password", ErrorMessage = "Passwords do not match!")]
-        public string ConfirmPassword { get; set; }
     }
 }
