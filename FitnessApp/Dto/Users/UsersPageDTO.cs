ed73fd32374b69791305312b3597b6737f24f10a
Djengiz Ibrqmov <djengizibrqmov@gmail.com>
2022-07-20 19:57:30 +0300

    Some controller modifications bcs of paging

---
+++ FitnessApp/Dto/Users/UsersPageDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace FitnessApp.Dto.Users
+{
+    public class UsersPageDTO
+    {
+        public ICollection<UserDTO> Users { get; set; }
+        public int PagesCount { get; set; }
+    }
+}
--- FitnessApp/Services/Data/IUsersService.cs
+++ FitnessApp/Services/Data/IUsersService.cs
@@ -5,7 +5,9 @@
 
     public interface IUsersService
     {
-        IEnumerable<UserDTO> GetUsers();
+        IEnumerable<UserDTO> GetUsers(string searchParams, int? take = null, int skip = 0);
+        int GetUsersCount();
+        int GetUsersCountBySearchParams(string searchParams);
         Task<UserDetailsDTO> GetUserByIdAsync(string id);
 
         Task<string> UpdateUserDetailsAsync(UpdateUserDetailsInputModel model, string userId);
