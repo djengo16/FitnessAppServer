7fa34fe0c54b0c341ba22b173090e5f1abec5ff8
Djengiz Ibrqmov <djengizibrqmov@gmail.com>
2022-07-22 15:39:51 +0300

    Implemented Builder pattern

---
+++ FitnessApp/Helper/ExerciseQueryBuilder.cs
@@ -0,0 +1,31 @@
+﻿namespace FitnessApp.Helper
+{
+    using FitnessApp.Models;
+
+    public class ExerciseQueryBuilder : QueryBuilder<Exercise>
+    {
+
+        public ExerciseQueryBuilder(IQueryable<Exercise> currentQuery) : base(currentQuery)
+        {
+            this.CurrentQuery = currentQuery;
+        }
+        public override IQueryable<Exercise> CurrentQuery { get; set; }
+        /// <summary>
+        /// Apply search to the given query if we have searchParams if not
+        /// we return true for every entity.
+        /// </summary>
+        /// <param name="searchParams">searchParameter used to check if entity params match with it</param>
+        /// <returns>QueryBuilder</returns>
+        public override QueryBuilder<Exercise> ApplySearch(string searchParams)
+        {
+            var muscleGroup = MuscleGroupFinder.FindMuscleGroup(searchParams);
+
+            this.CurrentQuery = CurrentQuery
+            .Where(x => !string.IsNullOrEmpty(searchParams)
+                            ? (x.Name.Contains(searchParams)
+                            || (int)x.MuscleGroup == muscleGroup)
+                            : true);
+            return this;
+        }
+    }
+}
