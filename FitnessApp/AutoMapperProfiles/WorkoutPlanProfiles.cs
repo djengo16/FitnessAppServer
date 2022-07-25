24ce86c8b4c78a71b30c1261920d2a589844dd69
Djengiz Ibrqmov <djengizibrqmov@gmail.com>
2022-07-25 11:55:40 +0300

    Change workout plan days mapping logic

--- FitnessApp/AutoMapperProfiles/WorkoutPlanProfiles.cs
+++ FitnessApp/AutoMapperProfiles/WorkoutPlanProfiles.cs
@@ -13,7 +13,7 @@
                 .ForMember(x => x.DaysInWeek, y => y.MapFrom(t => t.WorkoutDays.Count));
             CreateMap<WorkoutPlan, UserWorkoutPlanInAllUserPlansDTO>()
                 .ForMember(x => x.Difficulty, y => y.MapFrom(t => t.Difficulty.ToString()))
-                .ForMember(x => x.WorkoutDays, y => y.MapFrom(t => t.DaysInWeek))
+                .ForMember(x => x.WorkoutDays, y => y.MapFrom(t => t.WorkoutDays.Count()))
                 .ForMember(x => x.Goal, y => y.MapFrom(t => t.Goal.ToString()));
 
         }
